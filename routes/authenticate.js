b54fa51a4e827d963c5bb95b7f9f98c2e0ae82ca
David Schoenbauer <dschoenbauer@gmail.com>
2023-11-28 14:25:00 -0600

    2-Update token email

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -3,6 +3,30 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const errorView = require("../view/errorView");
 const MessageCode = require("../enum/MessageCode");
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const Email = require("../model/Email");
+const ejs = require("ejs")
+const fs = require("fs");
+
+const {
+	JWT_SECRET
+} = process.env;
+
+router.get("/reset/:email", (req, res, next) => {
+	const { params: { email } } = req;
+	const { pool } = res;
+	const text = `select id, email, fname, lname from pf.users where email = $1`;
+	pool.query({ text, values: [email], name: 'reset-password' }, (error, response) => {
+		if (response.rows.length === 0) return res.json({ success: true })
+		const { rows: [{ id, email, fname, lname } = { id: false }] } = response;
+		const token = jwt.sign({ id }, JWT_SECRET, { expiresIn: '24h' })
+		const body = ejs.render(fs.readFileSync('view/email/confirmEmailTemplate.ejs', 'utf-8'), { fname, lname, id, token, link: "" })
+		return Email({ to: email, subject: "Reset password", body }).then(() => {
+			return res.json({ success: true })
+		});
+	})
+})
 
 router.post("/login", (req, res, next) => {
 	const { body: { username, password } } = req;
@@ -12,7 +36,7 @@ router.post("/login", (req, res, next) => {
 		const { rows: [{ password: passwordHash } = { password: false }] } = response;
 		if (error || passwordHash === false) return errorView({ res, error, errorCode: 403, messageCode: MessageCode.USER_UNKNOWN });
 		bcrypt.compare(password, passwordHash, function (err, result) {
-			if(!result)  return errorView({ res, error, errorCode: 403, messageCode: MessageCode.USER_UNKNOWN });
+			if (!result) return errorView({ res, error, errorCode: 403, messageCode: MessageCode.USER_UNKNOWN });
 			res.json({ result })
 		});
 	});
